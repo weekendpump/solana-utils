fb8a25157f782481012cee7177a9bdde5731f73c
Weekend Pump Team <team.weekendpump.com>
2023-01-11 17:43:46 +0800

    0.0.1 successfuly published

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 
-const name = 'index';
+const name = 'solana-utils';
 
 export default {
   input: 'src/index.ts',
