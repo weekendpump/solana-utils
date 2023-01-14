4387cd56c389a30922149f1781f4028a14427422
Weekend Pump Team <team.weekendpump.com>
2023-01-14 20:02:39 +0800

    added missing exports for new services

--- src/services/solana/index.ts
+++ src/services/solana/index.ts
@@ -2,3 +2,5 @@ export * from './solana-api.service';
 export * from './solana-hash.service';
 export * from './solana-lookup.service';
 export * from './solana-rpc.service';
+export * from './solana-tx.service';
+export * from './spl-token.service';
